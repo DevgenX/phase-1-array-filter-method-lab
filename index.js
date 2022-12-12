a61b1827363ff5fb1934892a1f5aa7379dc781b7
devgenx <sebastian110420@gmail.com>
2022-12-12 14:41:09 -0500

    commit tree 71c687c831a99c5129c3899ea8bf5c36749c8108

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+const findMatching = (drivers, name) =>
+  drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
+const fuzzyMatch = (drivers, str) =>
+  drivers.filter(driver => driver.charAt(0) === str[0]);
+
+const matchName = (drivers, str) =>
+  drivers.filter(driver => driver.name === str);
